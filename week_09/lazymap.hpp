52db8f45e261728b1348facfbe68a41914e39f72
Boris Vassilev <boris.n.vassilev@gmail.com>
2026-05-15 11:55:35 +0300

    week 13 problems + old solutions

---
+++ week_09/lazymap.hpp
@@ -0,0 +1,41 @@
+#pragma once
+
+#include <functional>
+#include <vector>
+
+template <class U, class V>
+class LazyMap {
+   public:
+	LazyMap(const std::vector<U> &source, std::function<V(U)> f) : source(source), f(f) {}
+
+	class iterator {
+		typename std::vector<U>::const_iterator it;
+		const std::function<V(U)>			   &f;
+
+	   public:
+		iterator(typename std::vector<U>::const_iterator it, const std::function<V(U)> &f) : it(it), f(f) {}
+
+		V operator*() const { return f(*it); }
+		// V *operator->() const {return &f(*it); }
+		bool operator!=(const iterator &other) const { return it != other.it; }
+		bool operator==(const iterator &other) const { return it == other.it; }
+
+		iterator &operator++() {
+			++it;
+			return *this;
+		}
+		iterator operator++(int) {
+			iterator old = *this;
+			operator++();
+			return old;
+		}
+	};
+
+	iterator begin() const { return iterator(source.begin(), f); }
+
+	iterator end() const { return iterator(source.end(), f); }
+
+   private:
+	const std::vector<U> &source;
+	std::function<V(U)>	  f;
+};
---
+++ week_09/main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+#include "lazymap.hpp"
+
+int square(int x) {
+	return x * x;
+}
+
+int main() {
+
+	std::vector<int> v = {1,2,3,4};
+	
+	//for(int x : LazyMap<int, int>(v, square)) {
+	//	std::cout << x << std::endl;
+	//}
+	
+	auto container = LazyMap<int, int>(v, square);
+	for(auto it = container.begin(); it != container.end(); ++it) {
+		std::cout << *it << std::endl;
+	}
+	return 0;
+}
